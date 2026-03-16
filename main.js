bc220596f1403e124135511a2bd9c8ca04bd452a
SaturnV62653 <galenstasonis@icloud.com>
2026-03-15 22:22:40 -0400

    update main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function switchTheme() {
+    const body = document.body;
+
+    if (body.classList.contains('dark')) {
+        body.classList.remove('dark');
+    } else {
+        body.classList.add('dark');
+    }
+}
