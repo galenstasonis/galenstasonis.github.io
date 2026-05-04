d25a76ec2c80555901594a6e2128d0b431fb79ac
SaturnV62653 <galenstasonis@icloud.com>
2026-05-04 15:53:52 -0400

    temporarily remove hamburgber menu, prepare for code cleanup

--- hamburger.js
+++
@@ -1,6 +0,0 @@
-const hamMenu = document.querySelector('.hamburger-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu');
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-});
